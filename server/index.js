dbec37da57a4c25dc2bde01091edaf427bde553e
wei kee haw <weikeehaw@weis-MacBook-Pro.local>
2018-12-21 09:14:39 +0800

    add client folder

---
+++ server/index.js
@@ -0,0 +1,17 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+import users from "./routes/users";
+
+let app = express();
+
+app.use(bodyParser.json());
+app.use("/api/users", users);
+
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
+
+app.listen(6060, () => {
+  console.log("Running on localhost: 6060");
+});
